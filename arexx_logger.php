e58d930a1d932750fe9cbe6254a53947625991d3
suuntala <kosmik@hub.mail.kapsi.fi>
2014-02-23 17:51:23 +0200

    Refactoring: moved model types and operations into model.php.

--- arexx_logger.php
+++ arexx_logger.php
@@ -1,28 +1,12 @@
 <?php
 
+include "model.php";
 include "config.php";
-
-class Measurement
-{
-	public $id;
-	public $device;
-	public $taken_utc_s;
-	public $value;
-}
-
-class Device
-{
-	public $id;
-	public $external_id;
-	public $type_id;
-	public $label;
-	public $last_measurement_utc_s;
-}
 
 function log_data($dbh, $measurement)
 {
-	write_data($dbh, $measurement);
-	update_device_ts($dbh, $measurement);
+	$dbh->write_data($measurement);
+	$dbh->update_device_ts($measurement);
 }
 
 function skip_logging($measurement)
@@ -42,41 +26,6 @@ function skip_logging($measurement)
 	return $skip;
 }
 
-function write_data($dbh, $measurement)
-{
-	$stmt = $dbh->prepare("insert into measurement (device_id, taken_utc_s, value) values (?, ?, ?)");
-	$stmt->bindParam(1, $measurement->device->id);
-	$stmt->bindParam(2, $measurement->taken_utc_s);
-	$stmt->bindParam(3, $measurement->value);
-	$stmt->execute();
-}
-
-function update_device_ts($dbh, $measurement)
-{
-	$stmt = $dbh->prepare("update device set last_measurement_utc_s = ? where id = ?");
-	$stmt->bindParam(1, $measurement->taken_utc_s);
-	$stmt->bindParam(2, $measurement->device->id);
-	$stmt->execute();
-}
-
-function find_device($dbh, $external_id, $type_id)
-{
-	$stmt = $dbh->prepare("select id, external_id, type_id, label, last_measurement_utc_s from device where external_id=? and type_id=?");
-	if ($stmt->execute(array($external_id, $type_id))) {
-		while ($row = $stmt->fetch()) {
-			$device = new Device();
-			$device->id = $row["id"];
-			$device->external_id = $row["external_id"];
-			$device->type_id = $row["type_id"];
-			$device->label = $row["label"];
-			$device->last_measurement_utc_s = $row["last_measurement_utc_s"];
-			return $device;
-		}
-	}
-	
-	return null;
-}
-
 function parse_measurement($dbh)
 {
 	$type = $_POST["type"];
@@ -85,7 +34,7 @@ function parse_measurement($dbh)
 	$timestamp = time();
 
 	$m = null;
-	$device = find_device($dbh, $id, $type);
+	$device = $dbh->find_device($id, $type);
 
 	if ($device != null) {
 		$m = new Measurement();
@@ -97,21 +46,13 @@ function parse_measurement($dbh)
 	return $m;	
 }
 
-function get_connection()
-{
-	global $DATABASE_URL;
-	$dbh = new PDO($DATABASE_URL);
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	return $dbh;
-}
-
 $key = $_POST["key"];
 if ($key != $LOGGER_PASSWORD)
 {
 	die();
 }
 
-$dbh = get_connection();
+$dbh = new LoggerDao();
 try {
 	$dbh->beginTransaction();
 	
---
+++ model.php
@@ -0,0 +1,82 @@
+<?php
+
+class Measurement
+{
+	public $id;
+	public $device;
+	public $taken_utc_s;
+	public $value;
+}
+
+class Device
+{
+	public $id;
+	public $external_id;
+	public $type_id;
+	public $label;
+	public $last_measurement_utc_s;
+}
+
+class LoggerDao
+{
+	private $dbh;
+
+	function __construct()
+	{
+		global $DATABASE_URL;
+		$this->dbh = new PDO($DATABASE_URL);
+		$this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+
+	public function beginTransaction()
+	{
+		$this->dbh->beginTransaction();
+	}	
+	
+	public function commit()
+	{
+		$this->dbh->commit();
+	}	
+	
+	public function rollBack()
+	{
+		$this->dbh->rollBack();
+	}	
+	
+	public function write_data($measurement)
+	{
+		$stmt = $this->dbh->prepare("insert into measurement (device_id, taken_utc_s, value) values (?, ?, ?)");
+		$stmt->bindParam(1, $measurement->device->id);
+		$stmt->bindParam(2, $measurement->taken_utc_s);
+		$stmt->bindParam(3, $measurement->value);
+		$stmt->execute();
+	}
+
+	public function update_device_ts($measurement)
+	{
+		$stmt = $this->dbh->prepare("update device set last_measurement_utc_s = ? where id = ?");
+		$stmt->bindParam(1, $measurement->taken_utc_s);
+		$stmt->bindParam(2, $measurement->device->id);
+		$stmt->execute();
+	}
+
+	public function find_device($external_id, $type_id)
+	{
+		$stmt = $this->dbh->prepare("select id, external_id, type_id, label, last_measurement_utc_s from device where external_id=? and type_id=?");
+		if ($stmt->execute(array($external_id, $type_id))) {
+			while ($row = $stmt->fetch()) {
+				$device = new Device();
+				$device->id = $row["id"];
+				$device->external_id = $row["external_id"];
+				$device->type_id = $row["type_id"];
+				$device->label = $row["label"];
+				$device->last_measurement_utc_s = $row["last_measurement_utc_s"];
+				return $device;
+			}
+		}
+
+		return null;
+	}
+}
+
+?>
