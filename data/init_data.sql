858c5c4871d38492a7b347ce68d4788b56c0bfff
suuntala <kosmik@hub.mail.kapsi.fi>
2014-02-23 20:04:46 +0200

    Cleaned up sql file.

--- data/init_data.sql
+++ data/init_data.sql
@@ -10,33 +10,3 @@ insert into device (external_id, type_id, label, filename) values ("18432", "1",
 insert into device (external_id, type_id, label, filename) values ("18432", "3", "rh 18432", "18432_rh.csv");
 insert into device (external_id, type_id, label, filename) values ("18448", "1", "temp 18448", "18448_temp.csv");
 insert into device (external_id, type_id, label, filename) values ("18448", "3", "rh 18448", "18448_rh.csv");
-
-/*
--rw-r--r-- 1 suniala suniala 4,6M helmi 22 15:59 arexx_10691.csv
--rw-r--r-- 1 suniala suniala   45 helmi 22 15:37 arexx_17670RH.csv
--rw-r--r-- 1 suniala suniala 6,4M helmi 22 15:59 arexx_17878.csv
--rw-r--r-- 1 suniala suniala 312K helmi 22 15:58 arexx_17878RH.csv
--rw-r--r-- 1 suniala suniala 6,1M helmi 22 15:58 arexx_18064.csv
--rw-r--r-- 1 suniala suniala 299K helmi 22 15:59 arexx_18064RH.csv
--rw-r--r-- 1 suniala suniala 4,5M helmi 22 15:55 arexx_18378.csv
--rw-r--r-- 1 suniala suniala 227K helmi 22 15:57 arexx_18378RH.csv
--rw-r--r-- 1 suniala suniala 4,7M helmi 22 15:59 arexx_18432.csv
--rw-r--r-- 1 suniala suniala 221K helmi 22 15:57 arexx_18432RH.csv
--rw-r--r-- 1 suniala suniala 5,3M helmi 22 15:58 arexx_18448.csv
--rw-r--r-- 1 suniala suniala 229K helmi 22 15:58 arexx_18448RH.csv
-*/
-
-/*
-10691	temp
-17670	rh
-17878	temp
-17878	rh
-18064	temp
-18064	rh
-18378	temp
-18378	rh
-18432	temp
-18432	rh
-18448	temp
-18448	rh
-*/
