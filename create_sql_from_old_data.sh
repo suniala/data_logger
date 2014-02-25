f75b7ed3d065768ad0de98c0115153efe3b3f12a
suuntala <kosmik@hub.mail.kapsi.fi>
2014-02-25 21:14:09 +0200

    Script now creates proper sql inserts. Also, measurement interval in script is 600 seconds.

--- create_sql_from_old_data.sh
+++ create_sql_from_old_data.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 input_dir=$1
+device_db=$2
+
+# 10 * 60 seconds
+measurement_interval_s=600
+
 output_dir=$(mktemp -d)
-#output_dir=/tmp/tmp.xXZ2iHhR7A
 sql_file=$output_dir/init_old_data.sql
-#echo '' > $sql_file
 
 echo "Start time: "$(date)
 echo "Output dir is $output_dir"
@@ -19,27 +22,44 @@ echo "Filtering and creating sql..."
 curr_ext_id=''
 curr_type_id=''
 curr_cutoff_ts=''
+curr_device_id=''
 
 while read line; do
 	fields=(${line//;/ })
 	line_ext_id=${fields[0]}
 	line_type_id=${fields[3]}
 	line_ts=${fields[1]}
+	line_value=${fields[4]}
 	
 	if [ "$curr_ext_id" != "$line_ext_id" ]; then
 		curr_ext_id=$line_ext_id
 		curr_cutoff_ts=''
+		curr_device_id=''
 		echo "New external id $curr_ext_id"
     fi
 	if [ "$curr_type_id" != "$line_type_id" ]; then
 		curr_type_id=$line_type_id
 		curr_cutoff_ts=''
+		curr_device_id=''
 		echo "New type id $curr_type_id"
+    fi
+    
+    if [ "$curr_device_id" = "" ]; then
+    	curr_device_id=$(sqlite3 -batch $device_db "select id from device where external_id=$line_ext_id and type_id=$line_type_id;")
+
+	    if [ "$curr_device_id" = "" ]; then
+	    	echo "No device id found for external id $line_ext_id and type id $line_type_id. Bailing out."
+	    	exit 1
+	    fi
+	    
+	    echo "Current device id is $curr_device_id"
     fi
     
     # If this line's ts is after the current cutoff ts we should output the line.
     if [[ "$line_ts" > "$curr_cutoff_ts" ]]; then
-    	echo $line_ts >> $output_dir/init_old_data.sql
+    	# example: insert into measurement (device_id, taken_utc_s, value) values (25, 1393152940, 1.23);
+		line_taken_utc_s=$(date -d "$line_ts" +%s)
+    	echo "insert into measurement (device_id, taken_utc_s, value) values ($curr_device_id, $line_taken_utc_s, $line_value);" >> $output_dir/init_old_data.sql
     	
     	# Signal the need for a new cutoff ts.
     	curr_cutoff_ts=''
@@ -48,8 +68,8 @@ while read line; do
     # Create new cutoff ts if necessary.
     if [ "$curr_cutoff_ts" = "" ]; then
 		# example: date -d "2013-01-08T03:04:05+02:00" +%Y-%m-%d"T"%H:%M:%S%z
-		# Add 1 hour = 3600 seconds
-    	curr_cutoff_ts=$(date -d "$line_ts + 3600 seconds" +%Y-%m-%d"T"%H:%M:%S%z)
+		# Add the measurement interval
+    	curr_cutoff_ts=$(date -d "$line_ts + $measurement_interval_s seconds" +%Y-%m-%d"T"%H:%M:%S%z)
     fi
 done < $output_dir/all_sorted.csv
 
